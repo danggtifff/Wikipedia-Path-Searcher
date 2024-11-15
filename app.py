63802d49bbca2d819d9e9e681aa05a16d896f677
Tiffany Dang <tiffanymdang@gmail.com>
2024-11-15 15:11:18 -0500

    bones of the app

--- app.py
+++ app.py
@@ -2,11 +2,19 @@
 # https://flask.palletsprojects.com/en/stable/installation/
 # and then run:
 # $ python -m flask run
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route("/", methods=["GET", "POST"])
 def main():
-    return "<p>Hello World!</p>"
+    if request.method == "POST":
+        start = request.form.get("start")
+        end = request.form.get("end")
+        # Add code here to perform the "degrees of separation" calculation.
+        result = f"Finding shortest paths from {start} to {end}..."
+        return render_template("index.html", result=result, start=start, end=end)
+    return render_template("index.html", result=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
